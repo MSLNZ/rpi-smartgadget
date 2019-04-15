00afa90f504f4f02833ce1e5553565c12d3f84b3
jborbely <joe.borbely@gmail.com>
2019-04-15 20:10:48 +1200

    update the docstrings for SmartGadgetClient

--- smartgadget/client.py
+++ smartgadget/client.py
@@ -7,18 +7,22 @@ class SmartGadgetClient(LinkedClient):
         """Connect to the Raspberry Pi and link with the SmartGadget
         :class:`~msl.network.service.Service`.
 
-        :param str hostname: The hostname or IP address of the Raspberry Pi.
-        :param kwargs: Keyword arguments that are passed to
-                       :func:`~msl.network.client.connect`.
+        :param kwargs: Keyword arguments that are passed to :func:`~msl.network.client.connect`.
         """
         super(SmartGadgetClient, self).__init__('SmartGadget', **kwargs)
 
     def disconnect(self):
+        """
+        Shut down the SmartGadget :class:`~msl.network.service.Service`
+        and the Network :class:`~msl.network.manager.Manager`.
+        """
         self.disconnect_service()
-        super(SmartGadgetClient, self).disconnect()
+        super().disconnect()
 
     def service_error_handler(self):
-        """Shut down the SmartGadget :class:`~msl.network.service.Service`
+        """
+        Shut down the SmartGadget :class:`~msl.network.service.Service`
         and the Network :class:`~msl.network.manager.Manager` if there was
-        an error."""
-        self.disconnect_service()
+        an error.
+        """
+        self.disconnect()
