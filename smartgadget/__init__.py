6c3b6122900e1c85d377adbcdfe6feb829fd806c
jborbely <joe.borbely@gmail.com>
2019-07-23 09:04:44 +1200

    create kill_manager() function

--- smartgadget/__init__.py
+++ smartgadget/__init__.py
@@ -3,6 +3,8 @@ from msl.network import manager, ssh
 from .service import SmartGadgetService
 from .client import SmartGadgetClient
 
+RPI_EXE_PATH = 'shtenv/bin/smartgadget'
+
 
 def connect(*, host='raspberrypi', rpi_username='pi', rpi_password=None, timeout=10, **kwargs):
     """Connect to the :class:`~.service.SmartGadgetService` on the Raspberry Pi.
@@ -16,12 +18,13 @@ def connect(*, host='raspberrypi', rpi_username='pi', rpi_password=None, timeout
     :return: A connection to the :class:`~.service.SmartGadgetService` on the Raspberry Pi.
     :rtype: :class:`~.client.SmartGadgetClient`
     """
-    console_script_path = '/home/{}/shtenv/bin/smartgadget'.format(rpi_username)
+    console_script_path = '/home/{}/{}'.format(rpi_username, RPI_EXE_PATH)
     ssh.start_manager(host, console_script_path, ssh_username=rpi_username,
                       ssh_password=rpi_password, timeout=timeout, as_sudo=True, **kwargs)
 
     kwargs['host'] = host
-    return SmartGadgetClient('SmartGadget', **kwargs)
+    c = SmartGadgetClient('SmartGadget', **kwargs)
+    return c
 
 
 def start_service_on_rpi():
@@ -37,3 +40,26 @@ def start_service_on_rpi():
         )
 
     manager.run_services(SmartGadgetService(), **kwargs)
+
+
+def kill_manager(*, host='raspberrypi', rpi_username='pi', rpi_password=None, timeout=10, **kwargs):
+    """Kill the Network :class:`~msl.network.manager.Manager` on the Raspberry Pi.
+
+    :param str host: The hostname or IP address of the Raspberry Pi.
+    :param str rpi_username: The username for the Raspberry Pi.
+    :param str rpi_password: The password for `rpi_username`.
+    :param float timeout: The maximum number of seconds to wait for the connection.
+    :param kwargs: Keyword arguments that are passed to :meth:`~paramiko.client.SSHClient.connect`.
+    """
+    ssh_client = ssh.connect(host, username=rpi_username, password=rpi_password, timeout=timeout, **kwargs)
+    lines = ssh.exec_command(ssh_client, 'ps aux | grep smartgadget')
+    pids = []
+    for line in lines:
+        if RPI_EXE_PATH in line:
+            pids.append(line.split()[1])
+    for pid in pids:
+        try:
+            ssh.exec_command(ssh_client, 'sudo kill -9 ' + pid)
+        except:
+            pass
+    ssh_client.close()
