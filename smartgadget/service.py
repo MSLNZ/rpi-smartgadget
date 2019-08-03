5075612474678dbe4d8e9edd419bcb7227ec97db
jborbely <joe.borbely@gmail.com>
2019-08-03 23:36:08 +1200

    fix bugs in the _process and _connect methods

--- smartgadget/service.py
+++ smartgadget/service.py
@@ -1,5 +1,5 @@
 """
-The base :class:`~msl.network.service.Service` class.
+Base class for a Smart Gadget :class:`~msl.network.service.Service`.
 """
 import subprocess
 from datetime import datetime
@@ -11,29 +11,37 @@ try:
 except ImportError:  # then not on the Raspberry Pi
     Scanner, BTLEDisconnectError = object, object
 
+from . import logger
+
 
 class SmartGadgetService(Service):
 
     def __init__(self, cls):
+        """Base class for a Smart Gadget :class:`~msl.network.service.Service`.
+
+        :param cls: :class:`~smartgadget.sht3x.SHT3XService` or :class:`~smartgadget.shtc1.SHTC1Service`
+        """
         super(SmartGadgetService, self).__init__(name=cls.DEVICE_NAME)
         self.DEVICE_NAME = cls.DEVICE_NAME
         self.CLS = cls
-        self._retries = 5
+        self._max_attempts = 5
+        self._retries_remaining = 0
         self._scanner = Scanner()
         self._gadgets_available = {}
         self._gadgets_connected = {}
 
-    def max_retries(self) -> int:
+    def max_attempts(self) -> int:
         """Returns the maximum number of times to try to connect or read/write data from/to a Smart Gadget.
 
         Since the Bluetooth connection can drop unexpectedly, this provides the opportunity
-        to re-connect or re-send a request to the Smart Gadget.
+        to re-connect or re-send a request to a Smart Gadget.
         """
-        return self._retries
+        return self._max_attempts
 
-    def set_max_retries(self, value: int):
-        """Set the maximum number of times to try to read/write data from/to a Smart Gadget."""
-        self._retries = int(value)
+    def set_max_attempts(self, value: int):
+        """Set the maximum number of times to try to connect or read/write data from/to a Smart Gadget."""
+        self._max_attempts = max(1, int(value))
+        logger.debug('The maximum number attempts has been set to {}'.format(self._max_attempts))
 
     def scan(self, timeout=10, passive=False) -> List[str]:
         """Scan for Smart Gadgets that are within Bluetooth range.
@@ -44,9 +52,11 @@ class SmartGadgetService(Service):
         :rtype: list of str
         """
         self._gadgets_available.clear()
+        logger.debug('Start scanning for Smart Gadgets...')
         for d in self._scanner.scan(timeout=timeout, passive=passive):
             if d.getValueText(d.COMPLETE_LOCAL_NAME) == self.DEVICE_NAME:
                 self._gadgets_available[d.addr] = d
+        logger.debug('Found {} Smart Gadgets'.format(len(self._gadgets_available)))
         return list(self._gadgets_available)
 
     def connect_gadget(self, mac_address: str, strict=True) -> bool:
@@ -60,11 +70,9 @@ class SmartGadgetService(Service):
         Establishing a Bluetooth connection to a Smart Gadget takes approximately 7 seconds.
         If you are only requesting data from a couple of Smart Gadgets then connecting to each
         Smart Gadget at the beginning of your script and then fetching data in a loop would
-        be more efficient if you want to fetch data as fast as possible (`fast` is used very
-        loosely here since it takes approximately 1.5 seconds to read data from a Smart Gadget
-        even if the Raspberry Pi already has a Bluetooth connection to it). However, there are
+        be more efficient if you want to fetch data as quickly as possible. However, there are
         hardware limits to how many Smart Gadgets can simultaneously have a Bluetooth connection
-        with the Raspberry Pi. So, there is a compromise between how `fast` your program can
+        with the Raspberry Pi. So, there is a compromise between how quickly your program can
         fetch data and how many Smart Gadgets you want to fetch data from.
 
         :param str mac_address: The MAC address of the Smart Gadget to connect to.
@@ -78,22 +86,9 @@ class SmartGadgetService(Service):
     def connect_gadgets(self, mac_addresses, strict=True) -> Tuple[list, list]:
         """Connect to the specified Smart Gadgets.
 
-        It is not necessary to call this method to connect to a Smart Gadget via Bluetooth
-        before fetching data from it. The Bluetooth connection will automatically be
-        created and destroyed when requesting information from the Smart Gadget if the
-        Bluetooth connection does not already exist.
+        See :meth:`.connect_gadget` for more details.
 
-        Establishing a Bluetooth connection to a Smart Gadget takes approximately 7 seconds.
-        If you are only requesting data from a couple of Smart Gadgets then connecting to each
-        Smart Gadget at the beginning of your script and then fetching data in a loop would
-        be more efficient if you want to fetch data as fast as possible (`fast` is used very
-        loosely here since it takes approximately 1.5 seconds to read data from a Smart Gadget
-        even if the Raspberry Pi already has a Bluetooth connection to it). However, there are
-        hardware limits to how many Smart Gadgets can simultaneously have a Bluetooth connection
-        with the Raspberry Pi. So, there is a compromise between how `fast` your program can
-        fetch data and how many Smart Gadgets you want to fetch data from.
-
-        :param list mac_addresses: A list of MAC addresses of the Smart Gadget to connect to.
+        :param list mac_addresses: A list of MAC addresses of the Smart Gadgets to connect to.
         :param bool strict: Whether to raise an error if a Smart Gadget could not be connected to.
         :return: A list of MAC addresses of the Smart Gadgets that were successfully connected to
                  and the MAC addresses of the Smart Gadgets that could not be connected to.
@@ -101,14 +96,16 @@ class SmartGadgetService(Service):
         """
         failed_connections = []
         for mac_address in mac_addresses:
-            if mac_address in self._gadgets_available:
-                try:
-                    self._gadgets_connected[mac_address] = self._connect(mac_address)[0]
-                except BTLEDisconnectError:
-                    if strict:
-                        raise
-                    else:
-                        failed_connections.append(mac_address)
+            self._retries_remaining = self._max_attempts
+            try:
+                self._gadgets_connected[mac_address] = self._connect(mac_address)
+            except BTLEDisconnectError as e:
+                if strict:
+                    logger.error(e)
+                    raise
+                else:
+                    logger.warning('Could not connect to {!r}'.format(mac_address))
+                    failed_connections.append(mac_address)
         return list(self._gadgets_connected), failed_connections
 
     def connected_gadgets(self) -> List[str]:
@@ -120,6 +117,7 @@ class SmartGadgetService(Service):
         gadget = self._gadgets_connected.pop(mac_address, None)
         if gadget:
             try:
+                logger.debug('Disconnecting from {!r}...'.format(mac_address))
                 gadget.disconnect()
             except:
                 pass
@@ -132,6 +130,7 @@ class SmartGadgetService(Service):
             except:
                 pass
         self._gadgets_connected.clear()
+        logger.debug('Disconnected from all Smart Gadgets')
 
     def temperature(self, mac_address: str) -> float:
         """Returns the temperature [degree C] for the specified MAC address."""
@@ -181,8 +180,9 @@ class SmartGadgetService(Service):
 
         This will disconnect all Smart Gadgets that are currently connected.
         """
+        logger.debug('Restarting bluetooth...')
         self.disconnect_gadgets()
-        subprocess.call(['sudo', 'systemctl', 'restart', 'bluetooth'])
+        subprocess.run(['sudo', 'systemctl', 'restart', 'bluetooth'], check=True)
 
     @staticmethod
     def rpi_date() -> str:
@@ -199,36 +199,43 @@ class SmartGadgetService(Service):
         to sync with an online NTP server. Does not set the time zone.
         """
         iso = datetime.fromisoformat(date)
+        logger.debug('Setting Raspberry Pi date to {}'.format(iso))
         subprocess.run(['sudo', 'date', '-s', iso.strftime('%a %d %b %Y %I:%M:%S %p')], check=True)
 
-    def _connect(self, mac_address, retries_remaining=None):
-        """Connect to the Smart Gadget."""
-        if retries_remaining is None:
-            retries_remaining = int(self._retries)
-        if mac_address in self._gadgets_connected:
-            gadget = self._gadgets_connected[mac_address]
-        else:
-            while True:
+    def _connect(self, mac_address):
+        """Connect to a Smart Gadget."""
+        gadget = self._gadgets_connected.get(mac_address)
+        if gadget is None:
+            device = self._gadgets_available.get(mac_address) or mac_address
+            while gadget is None:
                 try:
-                    if mac_address in self._gadgets_available:
-                        # then we'll have the rssi value since we have the bluepy.btle.ScanEntry
-                        gadget = self.CLS(self._gadgets_available[mac_address])
-                    else:
-                        gadget = self.CLS(mac_address)
-                    break
-                except BTLEDisconnectError:
-                    if retries_remaining < 1:
+                    self._retries_remaining -= 1
+                    logger.debug('Connecting to {!r}...'.format(mac_address))
+                    gadget = self.CLS(device)
+                except BTLEDisconnectError as e:
+                    if self._retries_remaining < 1:
+                        logger.error(e)
                         raise
-                    retries_remaining -= 1
-        return gadget, retries_remaining
+                    text = 'retry remains' if self._retries_remaining == 1 else 'retries remaining'
+                    logger.warning('{} -- {} {}'.format(e, self._retries_remaining, text))
+        return gadget
 
     def _process(self, method_name, mac_address, **kwargs):
-        retries_remaining = int(self._retries)
+        logger.debug('Processing {!r} from {!r} -- kwargs={}'.format(method_name, mac_address, kwargs))
+        self._retries_remaining = self._max_attempts
+        add_to_gadgets_connected = False
         while True:
+            gadget = self._connect(mac_address)
+            if add_to_gadgets_connected:
+                self._gadgets_connected[mac_address] = gadget
+                logger.info('MAC address {!r} has been reconnected'.format(mac_address))
+
             try:
-                gadget, retries_remaining = self._connect(mac_address, retries_remaining=retries_remaining)
                 return getattr(gadget, method_name)(**kwargs)
-            except BrokenPipeError:
-                if retries_remaining < 1:
+            except (BrokenPipeError, BTLEDisconnectError) as e:
+                if self._retries_remaining < 1:
+                    logger.error(e)
                     raise
-                retries_remaining -= 1
+                add_to_gadgets_connected = self._gadgets_connected.pop(mac_address, None) is not None
+                text = 'retry remains' if self._retries_remaining == 1 else 'retries remaining'
+                logger.warning('{} -- {} {}'.format(e, self._retries_remaining, text))
