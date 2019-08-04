24d2c0c4d2c73dc6b99ec693f064089c0a84fd3b
jborbely <joe.borbely@gmail.com>
2019-08-04 16:52:04 +1200

    add ApiDocs and BuildDocs commands to setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,134 @@
+import re
+import sys
 import platform
 from setuptools import setup
+from distutils.cmd import Command
 
-on_rpi = platform.machine().startswith('arm')
+if sys.version_info[:2] < (3, 5):
+    sys.exit('Python < 3.5 is not supported because the asyncio package is required')
 
 install_requires = ['msl-network>=0.4']
-if on_rpi:
+if platform.machine().startswith('arm') or sys.platform.startswith('linux'):
     install_requires.append('bluepy')
+
+
+class ApiDocs(Command):
+    """
+    A custom command that calls sphinx-apidoc
+    see: https://www.sphinx-doc.org/en/latest/man/sphinx-apidoc.html
+    """
+    description = 'builds the api documentation using sphinx-apidoc'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        command = [
+            None,  # in Sphinx < 1.7.0 the first command-line argument was parsed, in 1.7.0 it became argv[1:]
+            '--force',  # overwrite existing files
+            '--module-first',  # put module documentation before submodule documentation
+            '--separate',  # put documentation for each module on its own page
+            '-o', './docs/_autosummary',  # where to save the output files
+            'smartgadget',  # the path to the Python package to document
+        ]
+
+        import sphinx
+        if sphinx.version_info < (1, 7):
+            from sphinx.apidoc import main
+        else:
+            from sphinx.ext.apidoc import main  # Sphinx also changed the location of apidoc.main
+            command.pop(0)
+
+        main(command)
+        sys.exit(0)
+
+
+class BuildDocs(Command):
+    """
+    A custom command that calls sphinx-build
+    see: https://www.sphinx-doc.org/en/latest/man/sphinx-build.html
+    """
+    description = 'builds the documentation using sphinx-build'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sphinx
+
+        command = [
+            None,  # in Sphinx < 1.7.0 the first command-line argument was parsed, in 1.7.0 it became argv[1:]
+            '-b', 'html',  # the builder to use, e.g., create a HTML version of the documentation
+            '-a',  # generate output for all files
+            '-E',  # ignore cached files, forces to re-read all source files from disk
+            'docs',  # the source directory where the documentation files are located
+            './docs/_build/html',  # where to save the output files
+        ]
+
+        if sphinx.version_info < (1, 7):
+            from sphinx import build_main
+        else:
+            from sphinx.cmd.build import build_main  # Sphinx also changed the location of build_main
+            command.pop(0)
+
+        build_main(command)
+        sys.exit(0)
+
+
+def read(filename):
+    with open(filename) as fp:
+        text = fp.read()
+    return text
+
+
+def fetch_init(key):
+    # open the __init__.py file to determine the value instead of importing the package to get the value
+    init_text = read('smartgadget/__init__.py')
+    return re.compile(r'{}\s*=\s*(.*)'.format(key)).search(init_text).group(1)[1:-1]
+
+
+testing = {'test', 'tests', 'pytest'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if testing else []
+
+needs_sphinx = {'doc', 'docs', 'apidoc', 'apidocs', 'build_sphinx'}.intersection(sys.argv)
+sphinx = ['sphinx', 'sphinx_rtd_theme'] if needs_sphinx else []
+
 
 setup(
     name='smartgadget',
-    version='0.1.0.dev0',
-    author='Joseph Borbely',
+    version=fetch_init('__version__'),
+    author=fetch_init('__author__'),
     author_email='joseph.borbely@measurement.govt.nz',
     url='https://github.com/MSLNZ/rpi-smartgadget',
     description='Communicate with a Sensirion SHTxx Smart Gadget',
-    long_description=open('README.rst').read().strip(),
+    long_description=read('README.rst'),
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: Linux',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
     ],
+    setup_requires=sphinx + pytest_runner + install_requires,
+    tests_require=['pytest-cov', 'pytest'],
     install_requires=install_requires,
+    cmdclass={'docs': BuildDocs, 'apidocs': ApiDocs},
     packages=['smartgadget'],
     entry_points={
         'console_scripts': [
