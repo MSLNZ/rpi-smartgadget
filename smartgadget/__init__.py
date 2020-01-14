b0d70577e2e82a8f31197e174bc7cf2fd9561e49
jborbely <joe.borbely@gmail.com>
2020-01-14 19:30:48 +1300

    update __copyright__ for 2020

--- smartgadget/__init__.py
+++ smartgadget/__init__.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from msl.network import manager, ssh
 
 __author__ = 'Joseph Borbely'
-__copyright__ = '\xa9 2019, ' + __author__
+__copyright__ = '\xa9 2019 - 2020, ' + __author__
 __version__ = '0.1.0.dev0'
 
 # if you change this value then you must also update the name of the
